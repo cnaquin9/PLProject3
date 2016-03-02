66d0e7f5342076cf0c8eee6304effe275af2f175
Carley Naquin <cnaquin9@yahoo.com>
2016-03-02 16:45:47 -0600

    idk

---
+++ Problem 1/subset_expansion.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $("#link").click(function(){
+        alert("click")
+    });})
+        /*$("div.hide").toggleClass("hidden");
+        if ($("div.hide").attr("class") != "hidden") {
+            $(this).hide();
+        }
+        else {
+            $(this).show();
+
+        }
+    }); // end click
+});
